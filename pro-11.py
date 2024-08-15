608ae70ee48c5bf7dd0b917a3e98e9fea0dbf77a
MR_PARMAR <miludhokdiya120@gmail.com>
2024-08-15 06:49:49 +0530

    Delete pro-11.py

--- pro-11.py
+++
@@ -1,8 +0,0 @@
-no=int(input("Enter any positive integer"))
-for i in range(2,int(no/2+1)):
-    if(no%i==0):
-        break
-if(i==int(no/2)):
-    print(no," number is Prime")
-else:
-    print(no," number is not Prime")
